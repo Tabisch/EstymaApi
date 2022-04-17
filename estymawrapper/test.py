6ff6029d6e8ee6ae57eba88e56b59c382350dfab
Tabisch <tobithieman@gmail.com>
2022-04-17 10:00:14 +0200

    yes4

--- estymawrapper/test.py
+++
@@ -1,9 +0,0 @@
-import json
-from api import EstymaApi
-
-credentials = json.load(open('credentials.json'))
-
-apisession = EstymaApi(credentials["username"],credentials["password"])
-
-for Device_Id in list(apisession.Devices.keys()):
-    print(json.dumps(apisession.fetchDevicedata(Device_Id), indent=4))
